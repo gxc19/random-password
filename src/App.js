bf51ab76d28d783b2b3ab091f115701103411894
Ga Ming Chan <gmchan@Gas-MacBook-Pro.local>
2020-10-02 16:08:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    letters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+    numbers: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
+    symbol: ["!", "@", "£", "$", "%", "^", "&", "*"],
+    counter: 0,
+    password: []
+  }
 
+  addHandler = () => {
+    this.setState({ counter: this.state.counter + 1 })
+  }
+
+  minusHandler = () => {
+    this.setState({ counter: this.state.counter - 1 })
+
+  }
+
+  ranLetter = () => {
+    
+  }
+
+
+  render() { 
+    return (
+      <div>
+        <h1>Random PassWord Generator</h1>
+        <h1>{this.state.password}</h1>
+        <button onClick={this.minusHandler}>-</button>
+        <h1>{this.state.counter}</h1>
+        <button onClick={this.addHandler}>+</button>
+        <button>Start</button>
+      </div>
+    );
+  }
+}
+ 
 export default App;

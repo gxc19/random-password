4f8a50905adf6931a4fa86294885e07aff7efa7e
Ga Ming Chan <gmchan@gas-macbook-pro.home>
2021-05-15 20:31:50 +0100

    working random characters

--- src/App.js
+++ src/App.js
@@ -4,33 +4,85 @@ import Button from './components/Button'
 
 class App extends Component {
   state = {
-    letters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-    numbers: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
-    symbol: ["!", "@", "£", "$", "%", "^", "&", "*"],
-    password: []
+    upper: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+    lower: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+    number: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
+    symbol: ["!", "@", "£", "$", "%", "^", "&", "*", "<", ">"],
+    password: [],
+    upperBtn: true,
+    lowerBtn: true, 
+    numberBtn: true,
+    symbolBtn: true,
+    counter: 1
   }
 
-  random1 = () => {
-    let letter = [...this.state.letters]
-    let rl = letter[Math.floor(Math.random()*letter.length)]
+    // Adds to the counter, but prevents you from going above 16
+    addHandler = () => {
+      if(this.state.counter === 16){
+          this.setState({ counter: 16 })
+      } else {
+          this.setState(prevState => ({ counter: prevState.counter + 1 }))
+      }
+    }
+  
+    // Decreases the counter, but prevents you from going below 0
+    minusHandler = () => {
+      if(this.state.counter === 1){
+          this.setState({ counter: 1 })
+      } else{
+          this.setState(prevState => ({ counter: prevState.counter - 1}))
+      }
+    }
 
-    this.setState({ password: rl})
+  start = () => {
+    let password = [...this.state.password]
+    let counter = this.state.counter
+
+    let upper = [...this.state.upper]
+    let lower = [...this.state.lower]
+    let number = [...this.state.number]
+    let symbol = [...this.state.symbol]
+
+    let characterOpt = [upper, lower, number, symbol]
+
+    for(let i = 0; i < counter; i++){
+      let currentOpt = characterOpt[Math.floor(Math.random()*characterOpt.length)]
+      let randomOpt = currentOpt[Math.floor(Math.random()*currentOpt.length)]
+      password.push(randomOpt) 
+    }
+    
+
+    this.setState({ password: password})
   }
 
-
-  render() { 
+  render() {
     return (
       <div className="container">
         <h1>Random PassWord Generator</h1>
-        <h1>
+        <div className="miniContainer">
+          <h1>
           {this.state.password}
-        </h1>
-        <Button/>
-        <h2>Upper Case</h2>
-        <h2>Lower Case</h2>
-        <h2>Numbers</h2>
-        <h2>Symbols</h2>
-        <button onClick={this.random1}>Start</button>
+          </h1>
+          <Button counter={this.state.counter} addHandler={this.addHandler} minusHandler={this.minusHandler}/>
+          <div className="optBtn">
+            <h2>Upper Case</h2>
+            <button>+</button>
+          </div>
+          <div className="optBtn">
+            <h2>Lower Case</h2>
+            <button>+</button>
+          </div>
+          <div className="optBtn">
+            <h2>Numbers</h2>
+            <button>+</button>
+          </div>
+          <div className="optBtn">
+            <h2>Symbols</h2>
+            <button>+</button>
+          </div>
+          <button onClick={this.start}>Start</button>
+        </div>
+        
       </div>
     );
   }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,34 +1,16 @@
-import React, {Component} from 'react'
-import '../style/Button.css'
+import React from "react";
+import "../style/Button.css";
 
-class Button extends Component {
-    state = {
-        counter: 1
-    }
-    
-    addHandler = () => {
-        this.setState({ counter: this.state.counter + 1 })
-      }
-    
-      minusHandler = () => {
-        if(this.state.counter === 1){
-            this.setState({ counter: 1 })
-        } else{
-            this.setState(prevState => ({ counter: prevState.counter - 1}))
-        }
-      }
+const Button = (props) => {
+  return (
+    <div className="button-container">
+      <button onClick={props.minusHandler}>-</button>
+      <div>
+        <h1>{props.counter}</h1>
+      </div>
+      <button onClick={props.addHandler}>+</button>
+    </div>
+  );
+};
 
-    render() { 
-        return (
-            <div className="button-container">
-                <button onClick={this.minusHandler}>-</button>
-                <div>
-                    <h1>{this.state.counter}</h1>
-                </div>
-                <button onClick={this.addHandler}>+</button>
-            </div>
-        );
-    }
-}
- 
 export default Button;
